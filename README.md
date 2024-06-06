9ce63009548c5db06a508b0464b4c806a7ceecfd
Piyush kumar <piyush2004.dev@gmail.com>
2024-06-06 20:28:23 +0530

    lecture 1 completed

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Wanderlust
+<!-- <div class="container">
+      <% for(let i = 0; i < allListings.length; i++){ %>
+      <a href="/listings/<%= allListings[i].id %>">
+        <div class="card">
+          <img
+            src="<%= allListings[i].image %>"
+            alt="<%= allListings[i].title %>"
+          />
+          <div class="card-content">
+            <p class="card-title"><%= allListings[i].title %></p>
+            <p class="card-price">₹<b><%= allListings[i].price%></b> / night</p>
+          </div>
+        </div>
+      </a>
+      <% } %>
+    </div> -->
--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
-const path = require("path")
+const path = require("path");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
@@ -21,17 +25,58 @@ main()
     console.error(err);
   });
 
-app.get("/", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("index.ejs", {allListings});
+// root
+app.get("/", (req, res) => {
+  res.send("HI, I'm root");
 });
 
-app.get("/:id", async (req, res) => {
+// index
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("index.ejs", { allListings });
+});
+
+// new
+app.get("/listings/new", async (req, res) => {
+  res.render("new.ejs");
+});
+
+// create
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+// show edit
+app.get("/listings/:id/edit", async (req, res)=>{
   const id = req.params.id;
   const listing = await Listing.findById(id);
-  console.log(listing);
-  res.render("show.ejs", { listing })
+  res.render("edit.ejs", { listing });
 });
+
+// edit
+app.put("/listings/:id", async (req, res)=>{
+  const id = req.params.id;
+  await Listing.findByIdAndUpdate(id, {...req.body.listing})
+  res.redirect(`/listings/${id}`);
+});
+
+// Delete
+app.delete("/listings/:id", async (req, res)=>{
+  const id = req.params.id;
+  const deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
+// show 
+app.get("/listings/:id", async (req, res) => {
+  const id = req.params.id;
+  const listing = await Listing.findById(id);
+  res.render("show.ejs", { listing });
+});
+
 
 app.listen(8080, () => {
   console.log("Listening on port http://localhost:8080");
